ea2980ae06ffe140cd90c2d478cbed125350f60b
liquidz <liquidz.uo@gmail.com>
2021-07-25 06:29:36 +0900

    Fix dep

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * as async from "https://deno.land/std@0.102.0/async/mod.ts";
-export * as bencode from "https://deno.land/x/deno_bencode@9.9.9/mod.ts";
+export * as bencode from "https://deno.land/x/deno_bencode@1.0.1/mod.ts";
 export * as bufio from "https://deno.land/std@0.102.0/io/bufio.ts";
