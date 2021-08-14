7e3782fc3098d945953d6074c3a0ce039e4f85d0
liquidz <liquidz.uo@gmail.com>
2021-08-15 06:14:24 +0900

    Bump deno-bencode

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * as async from "https://deno.land/std@0.104.0/async/mod.ts";
-export * as bencode from "https://deno.land/x/deno_bencode@1.0.1/mod.ts";
+export * as bencode from "https://deno.land/x/deno_bencode@1.0.2/mod.ts";
 export * as bufio from "https://deno.land/std@0.104.0/io/bufio.ts";
