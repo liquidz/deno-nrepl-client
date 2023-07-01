446eeaecc48f1774bc8cac16484cea077b449859
liquidz <liquidz.uo@gmail.com>
2023-07-02 07:02:53 +0900

    feat: Update connect function

--- nrepl.ts
+++ nrepl.ts
@@ -4,12 +4,16 @@ import { NreplClient } from "./types.ts";
 /**
  * Connect to a nREPL server.
  */
-export async function connect(
-  { hostname, port }: { hostname?: string; port: number },
-): Promise<NreplClient> {
+export async function connect({
+  hostname,
+  port,
+}: {
+  hostname?: string;
+  port: number;
+}): Promise<NreplClient> {
   const conn = await Deno.connect({
     hostname: hostname || "127.0.0.1",
     port: port,
   });
-  return new NreplClientImpl({ conn: conn });
+  return new NreplClientImpl({ conn });
 }
