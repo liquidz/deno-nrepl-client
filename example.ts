dd323b42e72d7079db5a67b7627c9981aeff43d5
liquidz <liquidz.uo@gmail.com>
2023-07-02 08:13:24 +0900

    docs: Add example.ts

---
+++ example.ts
@@ -0,0 +1,25 @@
+import * as nrepl from "./mod.ts";
+
+// Run `deno task example-nrepl-server` before running this example.
+const conn = await nrepl.connect({ port: 12345 });
+const closed = conn.start();
+
+// Write a request and read a response
+const resp = await conn.write({
+  op: "eval",
+  code: `(do (println "hello") (+ 1 2))`,
+});
+console.log(`result => ${resp.getOne("value")}`);
+
+// Fetch standard outputs/errors from stream
+Promise.race([
+  (async () => {
+    for await (const o of conn.output) {
+      console.log(`${o.type} => ${o.text}`);
+    }
+  })(),
+  closed,
+]);
+
+conn.close();
+await closed;
