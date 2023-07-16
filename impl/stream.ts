e2eb41aacdc6dec276029785ee644e33157ad4b2
liquidz <liquidz.uo@gmail.com>
2023-07-16 17:46:23 +0900

    feat: Add AssociatingContextStream

--- impl/stream.ts
+++ impl/stream.ts
@@ -1,8 +1,37 @@
 import { bencode } from "../deps.ts";
-import { NreplOutput } from "../types.ts";
+import { BencodeWithContext, NreplOutput, RequestManager } from "../types.ts";
 
-export class BencodeObjectToNreplOutputStream extends TransformStream<
+export class AssociatingContextStream extends TransformStream<
   bencode.BencodeObject,
+  BencodeWithContext
+> {
+  #reqManager: RequestManager;
+  constructor(reqManager: RequestManager) {
+    super({
+      transform: (chunk, controller) => {
+        this.#handle(chunk, controller);
+      },
+    });
+
+    this.#reqManager = reqManager;
+  }
+
+  #handle(
+    chunk: bencode.BencodeObject,
+    controller: TransformStreamDefaultController<BencodeWithContext>,
+  ) {
+    const id = chunk["id"];
+    if (id == null || typeof id !== "string") {
+      controller.enqueue({ message: chunk, context: undefined });
+    } else {
+      const reqBody = this.#reqManager[id];
+      controller.enqueue({ message: chunk, context: reqBody?.context });
+    }
+  }
+}
+
+export class BencodeWithContextToNreplOutputStream extends TransformStream<
+  BencodeWithContext,
   NreplOutput
 > {
   constructor() {
@@ -14,21 +43,24 @@ export class BencodeObjectToNreplOutputStream extends TransformStream<
   }
 
   #handle(
-    chunk: bencode.BencodeObject,
+    chunk: BencodeWithContext,
     controller: TransformStreamDefaultController<NreplOutput>,
   ) {
-    if (chunk["out"] != null && typeof chunk["out"] === "string") {
-      controller.enqueue({ type: "out", text: chunk["out"] });
+    const { message, context } = chunk;
+
+    if (message["out"] != null && typeof message["out"] === "string") {
+      controller.enqueue({ type: "out", text: message["out"], context });
     } else if (
-      chunk["pprint-out"] != null &&
-      typeof chunk["pprint-out"] === "string"
+      message["pprint-out"] != null &&
+      typeof message["pprint-out"] === "string"
     ) {
       controller.enqueue({
         type: "pprint-out",
-        text: chunk["pprint-out"],
+        text: message["pprint-out"],
+        context,
       });
-    } else if (chunk["err"] != null && typeof chunk["err"] === "string") {
-      controller.enqueue({ type: "err", text: chunk["err"] });
+    } else if (message["err"] != null && typeof message["err"] === "string") {
+      controller.enqueue({ type: "err", text: message["err"], context });
     }
   }
 }
