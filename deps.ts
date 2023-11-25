c85f9aa8bd88986d5ede9a69c1bc68a35c29e7b9
liquidz <liquidz.uo@gmail.com>
2023-11-26 06:58:42 +0900

    deps: Bump outdated libraries

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as async from "https://deno.land/std@0.192.0/async/mod.ts";
-export * as bencode from "https://deno.land/x/deno_bencode@1.1.0/mod.ts";
-export * as mockConn from "https://deno.land/std@0.192.0/http/_mock_conn.ts";
+export * as async from "https://deno.land/std@0.208.0/async/mod.ts";
+export * as bencode from "https://deno.land/x/deno_bencode@1.1.1/mod.ts";
+export * as mockConn from "https://deno.land/std@0.208.0/http/_mock_conn.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -1,3 +1,3 @@
-export * as asserts from "https://deno.land/std@0.192.0/testing/asserts.ts";
-export * as io from "https://deno.land/std@0.192.0/io/mod.ts";
-export * as streams from "https://deno.land/std@0.192.0/streams/mod.ts";
+export * as asserts from "https://deno.land/std@0.208.0/testing/asserts.ts";
+export * as io from "https://deno.land/std@0.208.0/io/mod.ts";
+export * as streams from "https://deno.land/std@0.208.0/streams/mod.ts";
